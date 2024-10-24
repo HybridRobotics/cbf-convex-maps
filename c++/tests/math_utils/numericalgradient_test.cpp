a50e3262a4cf98398813643f4af2887308451101
AkshayThiru <akshay_t@berkeley.edu>
2024-10-24 15:41:04 -0700

    Refactor math utils test files

---
+++ c++/tests/math_utils/numericalgradient_test.cpp
@@ -0,0 +1,76 @@
+#include <gtest/gtest.h>
+
+#include <Eigen/Core>
+#include <cmath>
+
+#include "sccbf/data_types.h"
+#include "sccbf/math_utils/numerical_derivatives.h"
+
+namespace {
+
+using namespace sccbf;
+
+TEST(NumericalGradient, ScalarExponential) {
+  const double k = 5;
+  const auto func = [k](const VectorXd& x) { return std::exp(k * x(0)); };
+  VectorXd x(6);
+  x << -1, -0.5, 0, 0.5, 1, 1.5;
+  VectorXd sol(x.rows());
+  sol.array() = (k * x).array().exp() * k;
+
+  for (int i = 0; i < x.rows(); ++i) {
+    VectorXd grad_(1);
+    VectorXd xi(1);
+    xi << x(i);
+    numerical_gradient(func, xi, grad_);
+    EXPECT_NEAR(grad_(0), sol(i), 1e-4)
+        << "Incorrect derivative at x = " << x(i);
+  }
+}
+
+TEST(NumericalGradient, ScalarLogarithm) {
+  const double k = 5;
+  const auto func = [k](const VectorXd& x) { return std::log(k * x(0)); };
+  VectorXd x(6);
+  x << 0.05, 0.1, 1, 10, 100, 1000;
+  VectorXd sol(x.rows());
+  sol = x.cwiseInverse();
+
+  for (int i = 0; i < x.rows(); ++i) {
+    VectorXd grad_(1);
+    VectorXd xi(1);
+    xi << x(i);
+    numerical_gradient(func, xi, grad_);
+    EXPECT_NEAR(grad_(0), sol(i), 1e-4)
+        << "Incorrect derivative at x = " << x(i);
+  }
+}
+
+TEST(NumericalGradient, VectorSinusoid) {
+  const double k1 = 5;
+  const double k2 = 10;
+  const double pi = static_cast<double>(EIGEN_PI);
+  const auto func = [k1, k2](const VectorXd& x) {
+    return std::sin(k1 * x(0)) * std::cos(k2 * x(1));
+  };
+  VectorXd x1(5);
+  x1 << 0, pi / 6, pi / 4, pi / 3, pi / 2;
+  VectorXd x2(x1.rows());
+  x2 = x1;
+
+  for (int i = 0; i < x1.rows(); ++i) {
+    for (int j = 0; j < x2.rows(); ++j) {
+      VectorXd grad_(2);
+      VectorXd xi(2);
+      xi << x1(i), x2(j);
+      numerical_gradient(func, xi, grad_);
+      VectorXd sol(2);
+      sol(0) = k1 * std::cos(k1 * xi(0)) * std::cos(k2 * xi(1));
+      sol(1) = -k2 * std::sin(k1 * xi(0)) * std::sin(k2 * xi(1));
+      EXPECT_NEAR((sol - grad_).lpNorm<Eigen::Infinity>(), 0, 1e-4)
+          << "Incorrect gradient at x = (" << x1(i) << ", " << x2(i) << ")";
+    }
+  }
+}
+
+}  // namespace
